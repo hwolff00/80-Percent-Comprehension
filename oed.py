6e2401cd25bc7eb7ff50452dfeb91538dfb6fc0f
hwolff00 <hwolff00@gmail.com>
2021-06-08 15:45:04 -0400

    new commit

--- oed.py
+++ oed.py
@@ -1,17 +1,21 @@
-#!/usr/local/bin/python
-# -*- coding: latin-1 -*-
-
+import configparser
 import requests
 import json
 import csv
 import re
+import os
+
 
 #-------------------------------------------------------------------------------
 # Fill in this area
-csv_name = "Gatsby_vocab.csv"
+csv_name = "nonsense.csv"
 word_lst = ['Break', 'Rise', 'Have', 'Meet', 'Keep', 'Ring', 'Forget', 'Drink', 'Become']
 #-------------------------------------------------------------------------------
 unparsed = []
+cfg = configparser.ConfigParser()
+cfg.read('zipf.cfg')
+
+app_key = cfg.get('KEYS', 'api_key', raw='')
 
 def filter(phrase):
     key = "([\d\w +-.''"",?!\(\):;]*){[\w/]*}([\d\w +-.''"",?!\(\);:]*)" #filter out extra unicode nonsence
@@ -30,7 +34,11 @@ def filter(phrase):
 with open(csv_name, 'a') as file:
     writer = csv.writer(file)
     for word in word_lst:
+<<<<<<< HEAD
         app_key = #fill in your app key here
+=======
+        # app_key = os.environ["od"]
+>>>>>>> e575472 (Added .gitignore file and fixed security issue in oed.py)
         query = word
 
 
