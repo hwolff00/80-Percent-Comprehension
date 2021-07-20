83c60d1f9c6ab352fd9d0f3fa55cb73d2370dbcc
hwolff00 <hwolff00@gmail.com>
2021-07-20 10:26:34 -0400

    Add todos and clean up parse_n_gram.py

--- scripts/parse_ngram.py
+++ scripts/parse_ngram.py
@@ -2,12 +2,17 @@ import pandas as pd
 
 word_dic={}
 
-with open("pnorvig_ngram.txt") as txt:
-    for x in range(10000):
-        line = txt.readline()
-        word, count = line.strip().split('\t')
-        word = word.capitalize()
-        word_dic[word] = count
 
-series = pd.Series(word_dic).to_frame()
-pd.DataFrame(series).to_csv('n_gram.csv')
+def main():
+    with open("pnorvig_ngram.txt") as txt:
+        for x in range(10000):
+            line = txt.readline()
+            word, count = line.strip().split('\t')
+            word = word.capitalize()
+            word_dic[word] = count
+
+    series = pd.Series(word_dic).to_frame()
+    pd.DataFrame(series).to_csv('n_gram.csv')
+
+if __name__ == "__main__":
+    main()
